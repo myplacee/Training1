31b864ba122d63f9c69b441cf6559e3e385416c1
ravikumar <ravicodeproject@gmail.com>
2018-09-25 02:03:36 +0530

    ConsoleApi_1
    ConsoleApi_1

---
+++ ConsoleApi_1/ConsoleApi_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Simple program for read and write operations on console
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApi_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi user please enter any string on console");
+            string input_from_user= Console.ReadLine();
+            
+            //bussiness logic area
+
+            Console.WriteLine("You entered string is " + input_from_user);
+            Console.ReadLine();
+        }
+    }
+}
